88d71ebd1ca2b8a75a9045e76e46bcf218661ebe
Chris Chen <chrisc@Chriss-MacBook-Pro-2.local>
2014-03-15 08:32:42 -0600

    Initial Commit

---
+++ a4q4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  a4q4
+//
+//  Created by Chris Chen on 2014-03-15.
+//  Copyright (c) 2014 Nuo Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
